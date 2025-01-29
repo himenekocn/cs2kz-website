0388c62d690f1f162dff405933656236f0509995
jonahbearde <bearinparadox@gmail.com>
2025-01-29 16:41:03 +0800

    fix(servers): tweak style on mobile

--- pages/servers/index.vue
+++ pages/servers/index.vue
@@ -4,7 +4,7 @@ const { servers, loading, error, query, total, getServers } = useServers()
 getServers()
 </script>
 <template>
-  <Main class="w-2/3 mx-auto">
+  <Main class="lg:w-2/3 mx-auto">
     <ServerQuery v-model:query="query" />
 
     <div v-if="total > 0" class="px-4 py-2 mt-8 border border-gray-700 rounded-md lg:mx-auto lg:w-max">
