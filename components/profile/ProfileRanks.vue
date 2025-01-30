b3ef4515f0c69b0d4bf06b141fd5033638dfe68a
jonahbearde <bearinparadox@gmail.com>
2025-01-30 18:57:36 +0800

    fix(profile): use gray color when number of top ranks is 0

--- components/profile/ProfileRanks.vue
+++ components/profile/ProfileRanks.vue
@@ -11,23 +11,23 @@ defineProps<{
   <div class="flex flex-wrap mb-4 gap-x-8 gap-y-0 lg:gap-y-8">
     <div class="flex">
       <span class="mr-1 text-gray-400">{{ $t("profile.completion.wrs") }}:</span>
-      <span class="font-semibold text-yellow-400">{{ wrs }}</span>
-      <IconMedalFirst class="w-5 h-5" />
+      <span class="font-semibold" :class="wrs > 0 ? 'text-yellow-400' : 'text-gray-500'">{{ wrs }}</span>
+      <IconMedalFirst v-if="wrs > 0" class="w-5 h-5" />
     </div>
 
     <div>
       <span class="mr-1 text-gray-400">{{ $t("profile.completion.top20") }}:</span>
-      <span class="font-medium text-red-400">{{ top20 }}</span>
+      <span class="font-medium" :class="top20 > 0 ? 'text-red-400' : 'text-gray-500'">{{ top20 }}</span>
     </div>
 
     <div>
       <span class="mr-1 text-gray-400">{{ $t("profile.completion.top50") }}:</span>
-      <span class="font-medium text-purple-400">{{ top50 }}</span>
+      <span class="font-medium" :class="top50 > 0 ? 'text-purple-400' : 'text-gray-500'">{{ top50 }}</span>
     </div>
 
     <div>
       <span class="mr-1 text-gray-400">{{ $t("profile.completion.top100") }}:</span>
-      <span class="font-medium text-orange-400">{{ top100 }}</span>
+      <span class="font-medium" :class="top100 > 0 ? 'text-orange-400' : 'text-gray-500'">{{ top100 }}</span>
     </div>
   </div>
 </template>
