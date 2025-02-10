5b7822d94ac8a743fc9d8fa89c300b10644b71e4
LynchMus <xubw@vip.qq.com>
2025-02-10 18:59:48 +0800

    Update Main.vue

--- components/Main.vue
+++ components/Main.vue
@@ -1,5 +1,12 @@
 <template>
   <main class="min-h-[calc(100vh-4rem)] px-4 lg:px-16 py-8">
+    <div class="px-4 py-2 mb-4 border border-gray-700 rounded-md lg:mx-auto lg:w-max">
+      <div class="flex items-center justify-center flex-wrap gap-4 text-gray-300">
+        <div class="flex items-center gap-2">
+            数据实时同步于 api.cs2kz.org
+        </div>
+      </div>
+      </div>
     <slot />
   </main>
 </template>
